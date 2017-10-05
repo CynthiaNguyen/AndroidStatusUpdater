602c5d552d1e783729ac291038e7eb52adf51674
Cynthia <radicalcyn@gmail.com>
2017-10-04 22:36:03 -0700

    Updated loop to update device status if there is a change

--- src/main/AndroidStatusUpdater.java
+++ src/main/AndroidStatusUpdater.java
@@ -3,6 +3,7 @@ package main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,52 +20,144 @@ public class AndroidStatusUpdater {
 		LOG.info("Testing log4j setup success!");
 		
 		//initialize device list
-		List<DeviceStatus> deviceStatuses = null;
+		List<DeviceStatus> previousStatuses = null;
 		List<DeviceStatus> currentStatuses = null;
 		int numOfDevices;
 		LOG.info("Initialized device list and number of devices");
 		
-		//initialize shell process
+		//initialize shell process variables
 		String line = "";
-		String command = "adb devices";
-		
+		String command = "adb devices";		
 		Runtime run = Runtime.getRuntime();
 		LOG.info("Running command = " + command);
-		try {
-			Process process = run.exec(command);//execute command
-			process.waitFor();//wait for process to finish and terminate
-			
-			//capture output from command
-			BufferedReader bf = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			LOG.info("-------------------------");
-			LOG.info("Output");
-			LOG.info("-------------------------");
-			
-			while ((line=bf.readLine()) != null) {
-				LOG.info(line);
-								
+		
+		//--------------------------
+		//	infinite loop
+		//--------------------------
+		while (true) {
+			try {
+				Process process = run.exec(command);//execute command
+				process.waitFor();//wait for process to finish and terminate
 				
-				//save current output
-				DeviceStatus currentDevice = parseAdbOutput(line);
+				currentStatuses = new ArrayList<DeviceStatus>();//refresh current device list				
+				BufferedReader bf = new BufferedReader(new InputStreamReader(process.getInputStream()));
 				
-				if (currentDevice != null) {
-					currentStatuses.add(currentDevice);
+				//--------------------------
+				//	save current output
+				//--------------------------
+				while ((line=bf.readLine()) != null) {
+					LOG.info(line);
+
+					DeviceStatus currentDevice = parseAdbOutput(line);					
+					if (currentDevice != null) {
+						currentStatuses.add(currentDevice);
+					}
+				}//end while
+				
+				//save total number of devices connected
+				numOfDevices = currentStatuses.size();
+				LOG.info("Number of devices connected = " + numOfDevices);
+				
+				//--------------------------
+				//	if no device is physically connected
+				//--------------------------
+				if (currentStatuses.size() == 0) {
+					//and there were devices connected before
+					if (previousStatuses != null) {
+						//update all those device statuses to "Disconnected"
+						for (int i = 0; i < previousStatuses.size(); i++) {
+							previousStatuses.get(i).setStatus("Disconnected");
+							String udid = previousStatuses.get(i).getUdid();
+							String status = previousStatuses.get(i).getStatus();
+							
+							LOG.info("Updating device = " + udid + " | status = " + status);								
+						}
+					} else {
+						LOG.info("No devices are connected! Waiting 30 seconds...");
+						Thread.sleep(5000);//wait 30 seconds
+						continue;//skip loop iteration and check adb again
+					}
 				}
 				
-				parseAdbOutput(line);
-				//parse line for udid and device status
+				//--------------------------
+				//	if this is the first time getting adb output, then update database for all devices connected
+				//--------------------------
+				if (previousStatuses == null) {
+					previousStatuses = currentStatuses;//initialize previousStatuses list
+
+					for (int i = 0; i < previousStatuses.size(); i++) {
+						String status = previousStatuses.get(i).getStatus();
+						String udid = previousStatuses.get(i).getUdid();
+
+						if (status.equalsIgnoreCase("offline")) {//if status = offline then update database as "Offline"
+							previousStatuses.get(i).setStatus("Offline");
+							status = previousStatuses.get(i).getStatus();
+							
+							LOG.info("Updating device = " + udid + " | status = " + status);
+						} else if (status.equalsIgnoreCase("device")) {//if status = device then update database as "Available"
+							previousStatuses.get(i).setStatus("Available");
+							status = previousStatuses.get(i).getStatus();
+							
+							LOG.info("Updating device = " + udid + " | status = " + status);								
+						}
+					}
+				}
 				
-				//save in device status list
+				//--------------------------
+				//	else update device status if current output status is different from before
+				//--------------------------
+				else {
+					//check if devices connected are still the same				
+					if (previousStatuses.size() == currentStatuses.size()) {
+						int size = previousStatuses.size();
+						
+						for (int i = 0; i < size; i++) {
+							String currentUdid = currentStatuses.get(i).getUdid();
+							String currentStatus = currentStatuses.get(i).getStatus();
+
+							String previousUdid = previousStatuses.get(i).getUdid();
+							String previousStatus = previousStatuses.get(i).getStatus();
+							
+							//if current device is same as previous device udid
+							if (currentUdid.equalsIgnoreCase(previousUdid)) {
+								
+								//and if device status changed then update database for this device
+								if (!currentStatus.equalsIgnoreCase(previousStatus)) {
+									if (currentStatus.equalsIgnoreCase("device")) {
+										previousStatuses.get(i).setStatus("Available");
+										previousStatus = previousStatuses.get(i).getStatus(); 
+									} else if (currentStatus.equalsIgnoreCase("offline")) {
+										previousStatuses.get(i).setStatus("Offline");
+										previousStatus = previousStatuses.get(i).getStatus();
+									}
+									LOG.info("Status changed for udid = " + currentUdid + "! Updating status = " + previousStatus);
+								}
+							}
+						}						
+					}
+				}
 				
-			}//end while
-			
-			bf.close();			
-		} catch (IOException e) {
-			LOG.error("Unable to execute command: " + command, e);			
-		} catch (InterruptedException e) {
-			LOG.error("Could not wait for process to terminate.", e);
-//			e.printStackTrace();
-		}
+				//--------------------------
+				//	if # of devices changed
+				//--------------------------
+				if (numOfDevices != previousStatuses.size()) {
+					//update the status of the disconnected device(s)
+					
+					LOG.info("Number of devices changed!");
+				}
+
+
+				
+				bf.close();							
+				LOG.info("Waiting 30 seconds...");
+				Thread.sleep(5000);//wait 30 seconds
+			} catch (IOException e) {
+				LOG.error("Unable to execute command: " + command, e);			
+			} catch (InterruptedException e) {
+				LOG.error("Could not wait for process to terminate.", e);
+//				e.printStackTrace();
+			}						
+		}//end outer while
 		
 	}//end main
 
@@ -81,9 +174,15 @@ public class AndroidStatusUpdater {
 			matcher = pattern.matcher(line);
 			
 			if (matcher.find()) {
-				LOG.info("udid = " + matcher.group(1));//return device
-				LOG.info("status = " + matcher.group(3));//return status
+				String udid = matcher.group(1);
+				String status = matcher.group(3);
 				
+				if (status.equalsIgnoreCase("offline")) {
+					status = "Offline";
+				} else if (status.equalsIgnoreCase("device")) {
+					status = "Available";
+				}
+//				LOG.info("udid = " + matcher.group(1) + " | status = " + matcher.group(3));				
 				return new DeviceStatus(matcher.group(1), matcher.group(3));
 			}//end if
 		}//end if
